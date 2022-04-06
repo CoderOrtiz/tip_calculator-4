806ce56050f1931eb9c4c1522fa84f2e924ba15d
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-04-06 16:09:17 -0400

    Solution created to coding challenge.

---
+++ script.js
@@ -0,0 +1,15 @@
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+
+const totals = [];
+
+    for(let i = 0; i < bills.length; i++){
+       
+    const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+
+        tips.push(calcTip(bills[i]));
+
+        totals.push(bills[i] + tips[i]);
+        console.log(`The bill was $${bills[i]}, the tip should be $${tips[i]}, which makes the grand total $${totals}.`)
+    }
